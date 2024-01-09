1cd768301f2dca6cb4c2c87f3e4b00147bf486af
AITELQUADI23 <148865560+AITELQUADI23@users.noreply.github.com>
2024-01-09 15:10:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors');
 const nodemailer = require('nodemailer');
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-// Configure Nodemailer with your email service details
+app.use(express.json());
+app.use(cors());
 
-app.post('/send-email',async (req, res) => {
-  const body=req.body
-  
+app.post('/send-email', async (req, res) => {
+  const body = req.body;
+
   // Create a Nodemailer transporter and send the email
- 
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -23,43 +20,38 @@ app.post('/send-email',async (req, res) => {
 
   const mailOptions = {
     from: 'AGADIR JOURNEY',
-    to:body.to,
-    subject:body.subject,
-    html:body.content,
+    to: body.to,
+    subject: body.subject,
+    html: body.content,
   };
 
-  transporter.sendMail(mailOptions, async(error, info) => {
+  transporter.sendMail(mailOptions, async (error, info) => {
     if (error) {
       console.log(error);
       res.status(500).send('Email not sent');
     } else {
-  
       console.log('Email sent: ' + info.response);
-      
-      const mailOptions = {
+
+      const mailOptionsClient = {
         from: 'AGADIR JOURNEY',
-        to:body.toClient,
-        subject:body.subjectClient,
-        html:body.contentClient,
+        to: body.toClient,
+        subject: body.subjectClient,
+        html: body.contentClient,
       };
-      transporter.sendMail(mailOptions, async(error, info) => {
-        if (error) {
-          console.log(error);
-          res.status(500).send('Email not sent');
+
+      transporter.sendMail(mailOptionsClient, async (errorClient, infoClient) => {
+        if (errorClient) {
+          console.log(errorClient);
+          res.status(500).send('Client email not sent');
         } else {
-      
-          console.log('Email sent: ' + info.response);
-          
-          
-          
+          console.log('Client email sent: ' + infoClient.response);
+          res.send('true');
         }
       });
-      
     }
   });
 });
 
 app.listen(3800, () => {
-  console.log('Server is running on port 3200');
-
+  console.log('Server is running on port 3800');
 });
